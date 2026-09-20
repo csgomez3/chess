eead4c8d016511e24851a295bffca9ace539ba54
Caden Gomez <csgomez@byu.edu>
2026-09-19 23:18:21 -0600

    update(p0): fixed king legal diagonal move logic

--- shared/src/main/java/chess/LegalMoveCheck.java
+++ shared/src/main/java/chess/LegalMoveCheck.java
@@ -67,9 +67,12 @@ public class LegalMoveCheck {
                         legalMoveList.add(new ChessMove(position, endPos, null));
                     }
                 }
-                if (beginRow > 1 && beginCol < 8) {
+                if (beginRow < 8 && beginCol > 1) {
                     endPos = diagonal(beginRow, beginCol, moveDist, 2);
-                    legalMoveList.add(new ChessMove(position, endPos, null));
+                    if (board.getPiece(endPos) == null ||
+                            board.getPiece(endPos).getTeamColor() != piece.getTeamColor()) {
+                        legalMoveList.add(new ChessMove(position, endPos, null));
+                    }
                 }
                 if (beginRow > 1 && beginCol > 1) {
                     endPos = diagonal(beginRow, beginCol, moveDist, 3);
@@ -78,9 +81,12 @@ public class LegalMoveCheck {
                         legalMoveList.add(new ChessMove(position, endPos, null));
                     }
                 }
-                if (beginRow < 8 && beginCol > 1) {
+                if (beginRow > 1 && beginCol < 8) {
                     endPos = diagonal(beginRow, beginCol, moveDist, 4);
-                    legalMoveList.add(new ChessMove(position, endPos, null));
+                    if (board.getPiece(endPos) == null ||
+                            board.getPiece(endPos).getTeamColor() != piece.getTeamColor()) {
+                        legalMoveList.add(new ChessMove(position, endPos, null));
+                    }
                 }
                 break;
             case QUEEN:
